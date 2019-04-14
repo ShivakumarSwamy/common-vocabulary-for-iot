c7123f6aff1d2e5c2f06a3ffea67edb7868af6ad
Shivakumar Swamy B G <shivakumarswamy.bg@gmail.com>
2019-04-15 01:32:28 +0200

    ADD: form exception

---
+++ forms/src/main/java/de/uni/stuttgart/ipvs/form/exception/FormControlInvalidException.java
@@ -0,0 +1,8 @@
+package de.uni.stuttgart.ipvs.form.exception;
+
+public class FormControlInvalidException extends Exception {
+
+    public FormControlInvalidException(String message) {
+        super(message);
+    }
+}
