828dc6d0c0600da30d505828d4b425c233e4ac68
Shivakumar Swamy B G <shivakumarswamy.bg@gmail.com>
2019-04-15 00:41:29 +0200

    ADD: vocabulary module, RDF issue #12

---
+++ vocabulary/README.md
@@ -0,0 +1,14 @@
+# Vocabulary module
+
+NOTE: 
+- This project would not packaged on it's own, needs to packaged via parent project
+- Only needed properties, class for project is present in the module
+
+### Version 0.1.0
+
+- [RDF](http://www.w3.org/1999/02/22-rdf-syntax-ns# "RDF")
+    - prefix label: `rdf`
+    - iri: `http://www.w3.org/1999/02/22-rdf-syntax-ns#`
+    
+    - Property
+        - type
---
+++ vocabulary/src/main/java/de/uni/stuttgart/ipvs/vocabulary/RDF.java
@@ -0,0 +1,25 @@
+package de.uni.stuttgart.ipvs.vocabulary;
+
+import de.uni.stuttgart.ipvs.sparql.prologue.PrefixDeclaration;
+import de.uni.stuttgart.ipvs.sparql.terminal.Iri;
+import de.uni.stuttgart.ipvs.sparql.terminal.PrefixLabel;
+import de.uni.stuttgart.ipvs.sparql.terminal.PrefixedName;
+
+public class RDF {
+
+    private static final Iri RDF_IRI;
+    private static final PrefixLabel RDF_PREFIX_LABEL;
+
+    public static final PrefixDeclaration RDF_PREFIX_DECLARATION;
+
+    public static final PrefixedName RDF_TYPE;
+
+    static {
+        RDF_IRI = new Iri("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+        RDF_PREFIX_LABEL = new PrefixLabel("rdf");
+
+        RDF_PREFIX_DECLARATION = new PrefixDeclaration(RDF_PREFIX_LABEL, RDF_IRI);
+
+        RDF_TYPE = new PrefixedName(RDF_PREFIX_LABEL, "type");
+    }
+}
