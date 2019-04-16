4d519bb50390e34d9b44d60713e47b99721cfea1
Shivakumar Swamy B G <shivakumarswamy.bg@gmail.com>
2019-04-16 11:43:56 +0200

    UPD: read file using resource as a stream

--- initial-load-vocabulary/src/main/java/de/uni/stuttgart/ipvs/ilv/FileUtils.java
+++ initial-load-vocabulary/src/main/java/de/uni/stuttgart/ipvs/ilv/FileUtils.java
@@ -2,14 +2,7 @@ package de.uni.stuttgart.ipvs.ilv;
 
 import lombok.NonNull;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 
 public class FileUtils {
 
@@ -18,26 +11,23 @@ public class FileUtils {
     }
 
 
-    public static String contentsAsStringFromFileInClasspath(@NonNull String filename) throws IOException {
+    public static String contentsAsStringFromFileInClasspath(@NonNull String filename) throws FileNotFoundException {
 
-        try {
-            URL fileUrl = getResourceUrlFromClasspath(filename);
-            Path filePath = getPathFromUrl(fileUrl);
-            return Files.readString(filePath);
+        StringBuilder content = new StringBuilder();
 
-        } catch (URISyntaxException | NullPointerException failedFileUrl) {
-            throw new FileNotFoundException(filename + " COULD NOT BE FOUND IN CLASSPATH");
+        try (InputStream inputStream = FileUtils.class.getClassLoader().getResourceAsStream(filename);
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) content.append(line);
+
+        } catch (IOException | NullPointerException failedFileRead) {
+            throw new FileNotFoundException(filename + " COULD BE FOUND");
         }
 
+        return content.toString();
     }
-
-    private static Path getPathFromUrl(URL url) throws URISyntaxException {
-        URI resourceURI = url.toURI();
-        return Paths.get(resourceURI);
-    }
-
-    private static URL getResourceUrlFromClasspath(String resourceName) {
-        return FileUtils.class.getClassLoader().getResource(resourceName);
-    }
-
 }
+
